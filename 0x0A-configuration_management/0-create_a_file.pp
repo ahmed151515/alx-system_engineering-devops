2e75497b2a78501cf8ea117685137a5c63f2da8c
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-02-23 21:23:56 +0200

    .

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+file { 'File':
+ensure  => file,
+path    => '/tmp/school',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet',
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,3 @@
+hello my name ahmed
+0x0A-configuration_management
+in this repo i will make bash
