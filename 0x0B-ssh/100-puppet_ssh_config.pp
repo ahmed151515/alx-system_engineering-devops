97048acd973e238a9de8a4871a78871b4f8ea72a
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-02-24 17:58:52 +0200

    .

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,10 @@
+# solev task
+file_line {'Turn off passwd auth':
+path      => /etc/ssh/ssh_config,
+file_line => 'PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/school',
+}
