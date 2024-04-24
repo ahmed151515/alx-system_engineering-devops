a784e59a1b0d6395cab2e0eee33be2a1f7c9155c
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-04-24 21:27:14 +0200

    .

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"Employee EMPLOYEE_NAME is done with tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):"
+"Employee EMPLOYEE_NAME \
+is done with tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):"
 
 import requests
 import sys
@@ -8,10 +9,11 @@ url = "https://jsonplaceholder.typicode.com"
 user = requests.get(url + f"/users/{sys.argv[1]}").json()
 todo = requests.get(url + f"/todos", params={"userId": sys.argv[1]}).json()
 
-completed_todo = [i for i in todo if i.get('completed') == True]
+completed_todo = [i for i in todo if i.get('completed') is True]
 
 print(
-    f"Employee {user.get('name')} is done with tasks({len(completed_todo)}/{len(todo)}):")
+    f"Employee {user.get('name')} \
+is done with tasks({len(completed_todo)}/{len(todo)}):")
 
 for i in completed_todo:
     print(f"\t{i.get('title')}")
