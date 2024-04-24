c9e906e087e0f6b76c11e368365a81bbfe5d22b3
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-04-24 22:08:49 +0200

    .

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -10,7 +10,6 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com"
     user = requests.get(url + f"/users/{sys.argv[1]}").json()
     todo = requests.get(url + f"/todos", params={"userId": sys.argv[1]}).json()
-    
     with open(f"{user.get('id')}.cvs", 'w') as f:
         for i in todo:
             f.write(f"\"{user.get('id')}\",\"{user.get('username')}\"\
