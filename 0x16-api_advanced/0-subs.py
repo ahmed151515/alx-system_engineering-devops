a25bed268c64da2c3bfdb098929598969e337c64
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-05-19 11:49:54 +0300

    .

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-"""Module for task 0"""
+"""_summary_
+
+    Returns:
+        _type_: _description_
+    """
+from requests import get
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
-    import requests
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
+    header = {'User-agent': 'My-User-Agent'}
+
+    res = get(
+        f"https://www.reddit.com/r/{subreddit}/about.json"
+        , headers=header, allow_redirects=False)
+    if res.status_code < 300:
+        res = res.json()
+        return res["data"]["subscribers"]
+    else:
         return 0
-
-    return sub_info.json().get("data").get("subscribers")
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""_summary_
+    """
+from requests import get
+import json
+
+
+def top_ten(subreddit):
+    header = {'User-agent': 'My-User-Agent'}
+
+    res = get(f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10",
+              headers=header, allow_redirects=False)
+    # return json.dumps(res, indent=4)
+    if res.status_code >= 300:
+        print('None')
+    else:
+        res = res.json()
+        posts = res["data"]["children"]
+        for p in posts:
+            print(p["data"]["title"])
