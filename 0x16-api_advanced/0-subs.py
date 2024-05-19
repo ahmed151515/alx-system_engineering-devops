370d2c57f53c47a0f2fb7cd4dbd4e25299df83ae
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-05-19 11:07:40 +0300

    .

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,7 +12,7 @@ def number_of_subscribers(subreddit):
     headers = {'User-agent': 'your bot 0.1'}
 
     res = get(
-        f"https://www.reddit.com/r/{subreddit}/about.json", headers=headers, allow_redirects=Falses)
+        f"https://www.reddit.com/r/{subreddit}/about.json", headers=headers, allow_redirects=False)
     if res.status_code == 200:
         res = res.json()
         return res["data"]["subscribers"]
