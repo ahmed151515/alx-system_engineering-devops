3e57d0baf9df48f6d5bf0ed4472137ce28d4f98a
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-05-19 11:11:21 +0300

    .

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,11 +9,11 @@ from requests import get
 
 def number_of_subscribers(subreddit):
 
-    headers = {'User-agent': 'your bot 0.1'}
+    headers = {'User-agent': 'My-User-Agent'}
 
     res = get(
         f"https://www.reddit.com/r/{subreddit}/about.json", headers=headers, allow_redirects=False)
-    if res.status_code == 200:
+    if res.status_code < 300:
         res = res.json()
         return res["data"]["subscribers"]
     else:
